b1dc6b74f6c7388252ad85c3a02b60ba15cdc8c2
Anas Baqai <anashabib139@gmail.com>
2023-06-12 22:48:10 +0500

    step-2 completed

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
 
 
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Women",
+    },
+    {
+      id: 5,
+      title: "Mens",
+    }
+  ]
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h1>Hats</h1>
-          <p>shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h1>Jackets</h1>
-          <p>shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h1>Sneakers</h1>
-          <p>shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h1>Womens</h1>
-          <p>shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h1>Mens</h1>
-          <p>shop Now</p>
-        </div>
-      </div>
-      
+      {
+        categories.map((category) => {
+          return (
+            <div className="category-container">
+            <div className="background-image"/>
+              <div className="category-body-container">
+                <h1>{category.title}</h1>
+                <p>shop Now</p>
+              </div>
+            </div>
+
+          )
+        })
+      }
+
+
     </div>
   );
 }
