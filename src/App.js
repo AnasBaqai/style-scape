95d23e7ceb8cab10c0c6df5818b92fe5d50bc37c
Anas Baqai <anashabib139@gmail.com>
2023-06-13 22:48:06 +0500

    step 3 done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
 import "./style.scss"
-
+import CategoriesContainer from "./Components/CatogoriesContainer/CategoriesContainer";
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Women",
-    },
-    {
-      id: 5,
-      title: "Mens",
-    }
-  ]
+ 
+  
   return (
-    <div className="categories-container">
-      {
-        categories.map((category) => {
-          return (
-            <div key={category.id} className="category-container">
-              <div className="background-image" />
-              <div className="category-body-container">
-                <h2>{category.title}</h2>
-                <p>shop Now</p>
-              </div>
-            </div>
-
-          )
-        })
-      }
-
-
-    </div>
+    <CategoriesContainer />
   );
 }
 
