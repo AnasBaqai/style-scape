dfa1cb7b7f4dd4abdf65958db91f332c3f059158
Anas Baqai <anashabib139@gmail.com>
2023-06-16 16:12:06 +0500

    examples of both signInWith redirect and SIgnInWithPopUp with google

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<NavigationBar/>}>
-        <Route index element={<Home/>} />
-        <Route path="sign-in" element={<Signin/>} />
+      <Route path="/" element={<NavigationBar />}>
+        <Route index element={<Home />} />
+        <Route path="sign-in" element={<Signin />} />
       </Route>
     </Routes>
   );
--- src/Components/sign-in/Sign-in.jsx
+++ src/Components/sign-in/Sign-in.jsx
@@ -1,21 +1,33 @@
-import { signInWithGooglePopUp, createUserDocument } from "../../utils/FireBase/firebase.utils"
+import { useEffect } from "react"
+import { signInWithGooglePopUp, createUserDocument, signInWithGoogleRedirect, auth } from "../../utils/FireBase/firebase.utils"
 
-
+import { getRedirectResult } from "firebase/auth"
 
 const Signin = () => {
 
-    const signInHandler =async ()=>{
-        try{
-            const {user} = await signInWithGooglePopUp()
-            createUserDocument(user);
-        }catch(err){
+    useEffect(() => {
+        const getRes = async () => {
+            const response = await getRedirectResult(auth);
+            if (response) {
+                const userDocRef = createUserDocument(response.user);
+            }
+        }
+        getRes();
+        }, [])
+    const signInHandler = async () => {
+        try {
+            const { user } = await signInWithGooglePopUp()
+            const userDocRef = createUserDocument(user);
+        } catch (err) {
             console.log(err);
         }
     }
+
     return (
         <div>
             <h1>Sign in</h1>
             <button onClick={signInHandler} >Sign In</button>
+            <button onClick={signInWithGoogleRedirect} >Sign In with redirect</button>
         </div>
     )
 }
