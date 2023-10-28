9e1d08339b2ebf7531917732649ac563d77607a5
anas baqai <anashabib139@gmail.com>
2023-10-28 10:46:35 -0700

    user selector created

--- src/Routes/NavigationBar/NavigationBar.jsx
+++ src/Routes/NavigationBar/NavigationBar.jsx
@@ -1,14 +1,15 @@
 import { Outlet} from "react-router-dom";
 import { Fragment, useContext } from "react";
+import { useSelector } from "react-redux";
 import { NavigationContainer,LogoContainer,Links,NavLink } from "./NavigationStyles";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import CartIcon from "../../Components/cartIcon/cartIcon.component";
 import CartDropDown from "../../Components/CartDropdown/CartDropdown.component";
-import { UserContext } from "../../Context/UserContext";
+import { selectCurrentUser } from "../../store/user/user.selector";
 import { signOutUser } from "../../utils/FireBase/firebase.utils";
 import { CartContext } from "../../Context/CartContext";
 const NavigationBar = () => {
-    const { currentUser } = useContext(UserContext);
+    const currentUser = useSelector(selectCurrentUser)
     const {isCartOpen,setIsCartOpen} = useContext(CartContext)
     const cartClickHandler = ()=>{
 
